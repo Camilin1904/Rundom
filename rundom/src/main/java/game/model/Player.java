3b1452359f8658ee1779ef73728eb5118c9fb831
Camilo Carmona Valencia <101128930+Camilin1904@users.noreply.github.com>
2022-11-24 11:08:38 -0500

    developed a way for the object player to move arround the map

--- rundom/src/main/java/game/model/Player.java
+++ rundom/src/main/java/game/model/Player.java
@@ -3,6 +3,7 @@ package game.model;
 public class Player implements Moveable{
     private static Player instance = new Player();
     private String dir;
+    private Vertex<String, Moveable> position;
 
     private Player(){}
 
@@ -10,9 +11,51 @@ public class Player implements Moveable{
         return instance;
     }
 
+    public int move(String dir){
+        this.dir = dir;
+        return move();
+    }
 
     @Override
     public int move() {
-        return 0;
+        Pair<Vertex<String, Moveable>,Integer> newPos = null;
+        switch(dir){
+            case ("R"):
+                if((newPos = position.getRight())!=null){
+                    position.setValue(null);
+                    position = newPos.getA();
+                    position.setValue(instance);
+                }
+                break;
+            case("L"):
+                if((newPos = position.getLeft())!=null){
+                    position.setValue(null);
+                    position = newPos.getA();
+                    position.setValue(instance);
+                }
+                break;
+            case("U"):
+                if((newPos = position.getUp())!=null){
+                    position.setValue(null);
+                    position = newPos.getA();
+                    position.setValue(instance);
+                }
+                break;
+            case("D"):
+                if((newPos = position.getDown())!=null){
+                    position.setValue(null);
+                    position = newPos.getA();
+                    position.setValue(instance);
+                }
+                break;
+
+        }
+        return newPos!=null?newPos.getB():-1;
+    }
+    public Vertex<String, Moveable> getPosition() {
+        return position;
+    }
+    public void setPosition(Vertex<String, Moveable> position) {
+        this.position = position;
     }
 }
